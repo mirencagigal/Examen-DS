6630609249d757f3ff27d89ec738eb258053a752
Miren <miren.cagigal@alumni.mondragon.edu>
2020-05-22 09:29:29 +0200

    guardo fichero.R

---
+++ Carga fichero.R
@@ -0,0 +1,7 @@
+load("~/Desktop/Examen_DS/Archivos_examen_ds/muestra12.RData")
+dim(muestra12)
+colnames(muestra12)
+
+#dimensiones: 342111 filas y 3 columnas
+# > dim(muestra12)
+# [1] 342111      3
